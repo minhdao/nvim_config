0d251b2b24bd709ce438c3b16541d7074620b9e0
minh <minh@minhs-MacBook-Pro.local>
2022-10-29 12:13:37 +0700

    Fix code to correctly install and source plugins when plugins-setup.lua is saved

--- lua/mdao/plugins-setup.lua
+++ lua/mdao/plugins-setup.lua
@@ -16,7 +16,7 @@ local packer_bootstrap = ensure_packer()
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
   augroup end
 ]])
 
